a482948bf1f4d21f89c61caae0cfb9e7fb94cece
Guilherme Marobo <guimarobo@gmail.com>
2025-05-15 21:59:27 -0300

    primeira atividade

--- NoArvore/src/Main.java
+++ NoArvore/src/Main.java
@@ -1,13 +1,3 @@
-class No{
-    String valor;
-    No esquerda, direita;
-
-    public No(String valor, No esquerda, No direita) {
-        this.valor = valor;
-        esquerda = direita = null;
-    }
-}
-
 public class Main{
     No raiz;
 }
