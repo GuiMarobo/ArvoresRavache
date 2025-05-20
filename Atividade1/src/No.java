54aa383b9a6f0e07db533d425a956d12f3f4aee6
Guilherme Marobo <guimarobo@gmail.com>
2025-05-20 19:24:23 -0300

    atividade1

---
+++ Atividade1/src/No.java
@@ -0,0 +1,9 @@
+public class No{
+    String valor;
+    No esquerda, direita;
+
+    public No(String valor, No esquerda, No direita) {
+        this.valor = valor;
+        esquerda = direita = null;
+    }
+}
