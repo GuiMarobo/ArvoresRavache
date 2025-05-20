f728cabf64faa3bea2529a70ba2c89680c6eefb2
Guilherme Marobo <guimarobo@gmail.com>
2025-05-20 20:06:49 -0300

    busca pré ordem + correção na class Arvore

--- Atividade1/src/Main.java
+++ Atividade1/src/Main.java
@@ -1,3 +1,5 @@
-public class Main{
-    No raiz;
+public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- Atividade1/src/No.java
+++ Atividade1/src/No.java
@@ -11,4 +11,14 @@ public class No{
         if (no == null) return 0;
         return 1 + contarNos(no.esquerda) + contarNos(no.direita);
     }
+
+    public void preOrdem(No no) {
+        if (no != null) {
+            System.out.println(no.valor + " ");
+            preOrdem(no.esquerda);
+            preOrdem(no.direita);
+
+        }
+    }
+
 }
