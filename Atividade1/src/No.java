335bfd8c52bcbddb94a61e059642a06a5917fea1
Guilherme Marobo <guimarobo@gmail.com>
2025-05-20 19:36:30 -0300

    segunda atividade

--- Atividade1/src/No.java
+++ Atividade1/src/No.java
@@ -6,4 +6,9 @@ public class No{
         this.valor = valor;
         esquerda = direita = null;
     }
+
+    public int contarNos(No no){
+        if (no == null) return 0;
+        return 1 + contarNos(no.esquerda) + contarNos(no.direita);
+    }
 }
