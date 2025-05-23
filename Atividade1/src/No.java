7f96a067b57e694c510ad8cd59de172b48e63d3a
Guilherme Marobo <guimarobo@gmail.com>
2025-05-22 21:00:41 -0300

    emOrdem

--- Atividade1/src/No.java
+++ Atividade1/src/No.java
@@ -22,4 +22,12 @@ public class No{
         }
     }
 
+    public void emOrdem(No no) {
+        if (no != null) {
+            emOrdem(no.esquerda);
+            System.out.print(no.valor + ", ");
+            emOrdem(no.direita);
+        }
+    }
+
 }
